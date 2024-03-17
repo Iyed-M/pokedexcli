cfbc5373d399b1161a0b578f896eae3dd124745c
Iyed-M <iyedmhiri01@gmail.com>
2024-03-17 17:16:57 +0100

    add AreaPokemons

--- api/types.go
+++ api/types.go
@@ -9,3 +9,12 @@ type locationAreas struct {
 		URL  string `json:"url,omitempty"`
 	} `json:"results,omitempty"`
 }
+
+type AreaPokemons struct {
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+	} `json:"pokemon_encounters"`
+}
