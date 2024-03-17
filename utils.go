6e9fcf3224c0d9d38b7f8011e4f78d994cebc6ea
Iyed-M <iyedmhiri01@gmail.com>
2024-03-17 17:19:54 +0100

    add parsing stdin for args

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strings"
 )
@@ -23,19 +24,39 @@ func getCmdFromStdin(commands map[string]clicommand) (clicommand, error) {
 		return commands["help"], nil
 	}
 	return command, nil
-
 }
 
-func cleanInput() (cmdName string, err error) {
+func cleanInput() (cmdName string, arg string, err error) {
+
 	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanLines)
 	scanner.Scan()
 	input := scanner.Text()
 
 	if err := scanner.Err(); err != nil {
-		return "", err
+		return "", "", err
 	}
 	if input == "" {
-		return "", nil
+		return "", "", nil
 	}
-	return strings.ToLower(input), nil
+
+	cmd, arg, err := parseInput(input)
+	if err != nil {
+		return "", "", err
+	}
+
+	fmt.Println("cmd : ", cmd)
+	fmt.Println("arg : ", arg)
+	return strings.ToLower(cmd), arg, nil
+}
+
+func parseInput(input string) (cmd string, arg string, err error) {
+	cmdAndArgs := strings.Split(input, " ")
+	if len(cmdAndArgs) == 2 {
+		return cmdAndArgs[0], cmdAndArgs[1], nil
+	}
+	if len(cmdAndArgs) == 1 {
+		return cmdAndArgs[0], "", nil
+	}
+	return "", "", fmt.Errorf("should have only one arg for got %v", len(cmdAndArgs)-1)
 }
