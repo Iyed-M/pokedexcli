7b404d1695b4453ed29053b136daaa214bd5b402
Iyed-M <iyedmhiri01@gmail.com>
2024-03-17 17:20:43 +0100

    ..

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 	"github.com/Iyed-M/pokedexcli/api"
 )
 
-const DEFAULT_URL = "https://pokeapi.co/api/v2/location-area"
+const DefaultLocaionsURL = "https://pokeapi.co/api/v2/location-area"
 
 func main() {
 	cfg := config{
-		nextLocationAreaURL: DEFAULT_URL,
+		nextLocationAreaURL: DefaultLocaionsURL,
 		client:              api.NewClient(100*time.Second, 5*time.Second),
 	}
 	startRepl(&cfg)
